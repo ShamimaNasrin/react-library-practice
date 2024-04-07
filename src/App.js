87d55d083b3c00e4d58ad90b44efabea4006115d
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-04-07 13:11:42 +0600

    organized the Ui

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import CustomTwo from './components/CustomTwo';
 import D3OrgTestOne from './components/D3OrgTestOne';
 import RootNode from './components/RootNode';
 import D3TreeTestOne from './components/D3TreeTestOne';
+import { useState } from 'react';
 
 function App() {
-  
-  const data = [
+  const [title, setTitle] = useState('Tree1');
+
+  const treeData = [
     { order: 'root', name: 'Jane Doe', empId: '0021', reportingManagerId: '0000', designation: 'CEO', photo: 'https://firebasestorage.googleapis.com/v0/b/pluxi-3716a.appspot.com/o/CL0001%2FBR0001%2FTraining%2FE001111%2FScreenshot%202024-02-02%20145755.png?alt=media', score: '50' },
     { order: 'child1', name: 'Tyler Weary', empId: '0721', reportingManagerId: '0021', designation: 'Devops', photo: '🤓', score: '90' },
     { order: 'child2', name: 'Kristi Price', empId: '0822', reportingManagerId: '0721', designation: 'Developer', photo: '🤩', score: '110' },
@@ -18,12 +20,27 @@ function App() {
     { order: 'child1', name: 'John', empId: '0651', reportingManagerId: '0021', designation: 'Developer', photo: '🤓', score: '90' },
     { order: 'child3', name: 'Alex', empId: '5021', reportingManagerId: '0822', designation: 'Soft Eng', photo: '🤩', score: '70' },
     { order: 'child3', name: 'Alen', empId: '0121', reportingManagerId: '0822', designation: 'QA', photo: '🤩', score: '110' },
-];
+  ];
 
 
 
   return (
     <div className="border w-100 p-5">
+
+      <div className='d-flex gap-1'>
+        <button className='btn btn-primary'
+          onClick={() => setTitle('Tree1')}
+        >Tree1</button>
+        <button className='btn btn-primary'
+          onClick={() => setTitle('Tree2')}
+        >Tree2</button>
+        <button className='btn btn-primary'
+          onClick={() => setTitle('Tree3')}
+        >Tree3</button>
+        <button className='btn btn-primary'
+          onClick={() => setTitle('Word')}
+        >Word Cloud</button>
+      </div>
 
       {/* <TestOne></TestOne> */}
       {/* <FlowTwo></FlowTwo> */}
@@ -31,7 +48,11 @@ function App() {
         data?.map((d, i) => <RootNode key={i} data={d}></RootNode>)
       } */}
 
-      <D3OrgTestOne data={data} ></D3OrgTestOne>
+      {title === 'Tree1' && <D3OrgTestOne data={treeData} />}
+      {title === 'Tree2' && <FlowTwo data={treeData} />}
+      {title === 'Tree3' && <TestOne data={treeData} />}
+      {title === 'Word' && <TestOne data={treeData} />}
+
 
 
       {/* <D3TreeTestOne ></D3TreeTestOne> */}
